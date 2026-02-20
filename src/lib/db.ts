8acc8797169ec29f94c99ac87508cb4e190ae540
vikas.deshmukh <vikas.deshmukh@esds.co.insource>
2026-02-20 16:29:07 +0530

    Add Prisma setup, UI components, and Tailwind CSS integration

---
+++ src/lib/db.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+function createPrismaClient() {
+  const connectionString = process.env.DATABASE_URL;
+  if (!connectionString) throw new Error("DATABASE_URL is not set");
+  const adapter = new PrismaPg({ connectionString });
+  return new PrismaClient({ adapter });
+}
+
+export const prisma =
+  globalForPrisma.prisma ?? (globalForPrisma.prisma = createPrismaClient());
